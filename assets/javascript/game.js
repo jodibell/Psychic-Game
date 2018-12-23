a6538acb9cb741f869189e049ef873997aa3c4b4
JODI BELL <jodibell@JODIs-MacBook.local>
2018-12-23 16:07:03 -0500

    fixed link to js and made more variables global

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,28 +4,44 @@ var wins = 0,
     losses = 0,
     guessesLeft = 5;
     
-var guessesSoFar = "";
+var guessesSoFar = [];
+
+// Get the computer's key choice. Must be random. Create an array with the choices and randomly choose one of the choices.
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//Guessing there's a better way to go through the alphabet than what I did above...
+
+var computerGuess = computerChoices [Math.floor(Math.random) * computerChoices.length];
 
 // Get user input via key press and store in a variable.
 // This function is run when a user presses a key.
 document.onkeyup = function(event) {
     // Determines which key was pressed. (May add something later if a non-letter key was pressed.)
+
+    //If guessesLeft > 0, then check that it's a letter key, and if so, run this function.
     var userGuess = event.key;
 
-    // Get the computer's key choice. Must be random. Create an array with the choices and randomly choose one of the choices.
-    var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    //Guessing there's a better way to go through the alphabet than what I did above...
-
-    var computerGuess = computerChoices [Math.floor(Math.random) * computerChoices.length];
-
+    
     //Compare userGuess and computerGuess with if statement to determine result.
     if (userGuess === computerGuess){
-        wins++
+        wins++;
+        console.log (wins);
         //Restart game or display Win message?
-        ;
+        
     } else {
         //Add userGuess to guessesSoFar. Decrease guessesLeft by 1.
+        guessesLeft--;
+        console.log(guessesLeft);
+
+    // When guessesLeft=0, losses++
     }
+    //Display results to DOM
+    var html = 
+    "<p> Wins = " + wins + "</p>" +
+    "<p> Losses = " + losses + "</p>" +
+    "<p> Guesses Left = " + guessesLeft + "</p>" +
+    "<p> Guesses So Far = " + guessesSoFar + "</p>";
+
+    document.getElementById("stats").innerHTML = html;
     
 }
 
